199441de2bbf2ae14b76b0baccdd57db98c8394a
souravSarkar78 <ss830897@gmail.com>
2021-06-12 02:05:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//alert("Me");
+// console.log("Hello world");
+// document.write("this is document write");
+
+// javascript console API
+// console.warn("this is warning");
+// console.error("this is error");
+
+// js variables
+
+// var n1 = 34;
+// var n2 = 10;
+// var arr = [1, 2, 3, 4, 5]
+// console.log(n1 + n2);
+// console.log(arr);
+
+// functions 
+
+function avg(a, b){
+    c = (a+b)/2;
+    return c;
+}
+
+c1 = avg(10, 20);
+console.log(c1);
