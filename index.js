743eae4d1d374b7089a195978e6e780eb4158413
souravSarkar78 <ss830897@gmail.com>
2021-06-12 21:23:44 +0530

    learned few more things

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-alert("Me");
+// alert("Me");
 // console.log("Hello world");
 // document.write("this is document write");
 
-// javascript console API
+// javascript console API //////////
 // console.warn("this is warning");
 // console.error("this is error");
 
-// js variables
+// js variables ///////////
 
 // var n1 = 34;
 // var n2 = 10;
@@ -14,12 +14,62 @@ alert("Me");
 // console.log(n1 + n2);
 // console.log(arr);
 
-// functions 
+// functions ////////////
 
-function avg(a, b){
-    c = (a+b)/2;
-    return c;
-}
+// function avg(a, b){
+//     c = (a+b)/2;
+//     return c;
+// }
 
-c1 = avg(10, 20);
-console.log(c1);
+// c1 = avg(10, 20);
+// console.log(c1);
+
+// IF - ELSE ////////////
+
+// var age = 25;
+
+// if(20 < age){
+//     console.log('Less than age! ');
+// }
+// else{
+//     console.log('greate than age! ')
+// }
+
+// Loop //////////////////
+
+var arr = [1, 2, 3, 4, 5, 6];
+  
+// for (var i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(element){
+//     console.log(element);
+// });
+
+let j = 1;
+
+// while (j< 15){
+//     console.log(j);
+//     j++;
+// }
+
+// do{
+//     console.log(arr[j]);
+//     j++;
+// }while(j<arr.length)
+
+// Array methods ////////////////
+
+// let myarr = [1, 5, 2, true, 'str'];
+// myarr.push('sourav'); 
+// myarr.unshift('sourav');
+// console.log(myarr.sort());
+
+// String methods ///////////////////
+
+let mystring = 'My name is sourav sarkar';
+
+console.log(mystring.length)
+
+console.log(mystring.indexOf('sarkar'))   // fing something in array
